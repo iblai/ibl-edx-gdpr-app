b1a6a1e28970860dc2dbc10be0e174d84f3358cf
Azeem <azeemanimashaun@gmail.com>
2021-08-09 23:16:39 +0100

    Update https

--- ibl_edx_gdpr/apps.py
+++ ibl_edx_gdpr/apps.py
@@ -8,9 +8,6 @@ class EdxGDPRConfig(AppConfig):
 
     def ready(self):
         from .management.commands.ibl_retirement_states import Command
-        # # Set Retirement User to IBLs
-        # from django.conf import settings
-        # from ibl_edx_gdpr.config import IBL_RETIREMENT_SERVICE_WORKER
-        # setattr(settings, 'RETIREMENT_SERVICE_WORKER_USERNAME', IBL_RETIREMENT_SERVICE_WORKER)
+
         command = Command()
         command.handle()
