448456baa190c5e2fb092354a757072741fd0930
Shinji <shinji@shinji.rocks>
2022-10-12 00:28:33 +0800

    add test

---
+++ tests/test_views_retirement.py
@@ -0,0 +1,51 @@
+import pytest
+from common.djangoapps.student.tests.factories import UserFactory
+from openedx.core.djangoapps.oauth_dispatch.tests.factories import (
+    AccessTokenFactory,
+    ApplicationFactory,
+)
+
+from .utils import get_authenticated_client_and_user, reverse
+
+
+@pytest.mark.django_db
+class TestViewsRetirement:
+    @property
+    def staff(self):
+        if hasattr(self, "_staff") is False:
+            self._staff = UserFactory(is_staff=True)
+        return self._staff
+
+    @property
+    def application(self):
+        if hasattr(self, "_application") is False:
+            self._application = ApplicationFactory.create(user=self.staff)
+        return self._application
+
+    @property
+    def token(self):
+        return AccessTokenFactory.create(
+            user=self.staff, application=self.application
+        ).token
+
+    def test_place_learner_in_retirement_pipeline_returns_200(self):
+        user = UserFactory()
+        client, _ = get_authenticated_client_and_user(user=self.staff)
+        data = {
+            "username": user.username,
+        }
+
+        resp = client.post(
+            reverse("ibl_edx_gdpr_place_in_retirements"),
+            data,
+            format="json",
+            HTTP_AUTHORIZATION=f"Bearer {self.token}",
+        )
+
+        assert resp.status_code == 200
+        assert resp.data["message"] == "{} added to retirements successfully".format(
+            user.username
+        )
+
+    def test_place_learner_in_retirement_pipeline_multiple_times_returns_400(self):
+        ...
---
+++ tests/utils.py
@@ -0,0 +1,22 @@
+from common.djangoapps.student.tests.factories import UserFactory
+from django.shortcuts import reverse as django_reverse
+from rest_framework.test import APIClient
+
+
+def get_authenticated_client_and_user(*args, **kwargs):
+    client = APIClient()
+    kwargs_user = kwargs.pop("user", None)
+    if not kwargs_user:
+        user = UserFactory(*args, **kwargs)
+    else:
+        user = kwargs_user
+    client.force_authenticate(user=user)
+    return client, user
+
+
+def reverse(name, args=None, kwargs=None):
+    if args is None:
+        args = []
+    if kwargs is None:
+        kwargs = []
+    return django_reverse(f"ibl_edx_gdpr:{name}", args=args, kwargs=kwargs)
